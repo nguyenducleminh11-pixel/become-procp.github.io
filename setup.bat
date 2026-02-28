87d09b9cde477a6209bf79209781cbd11655e3f3
nguyenducleminh11-pixel <nguyenducleminh11@gmail.com>
2026-02-28 20:43:31 +0700

    Add setup script to create algorithm directories and files
    This script creates directories for various algorithm categories and initializes HTML files for each algorithm.

---
+++ setup.bat
@@ -0,0 +1,40 @@
+@echo off
+
+mkdir math sorting ds dp graph string advanced geometry greedy recursion basic
+
+echo.> math/gcd.html
+echo.> math/binary-exp.html
+echo.> math/sieve.html
+echo.> math/ncr.html
+
+echo.> sorting/merge.html
+echo.> sorting/quick.html
+echo.> sorting/binary-search.html
+
+echo.> ds/stack.html
+echo.> ds/queue.html
+echo.> ds/dsu.html
+echo.> ds/segment-tree.html
+echo.> ds/fenwick.html
+
+echo.> dp/knapsack.html
+echo.> dp/lis.html
+echo.> dp/lcs.html
+echo.> dp/bitmask.html
+
+echo.> graph/bfs.html
+echo.> graph/dfs.html
+echo.> graph/dijkstra.html
+echo.> graph/topo.html
+echo.> graph/scc.html
+echo.> graph/mst.html
+echo.> graph/maxflow.html
+
+echo.> string/kmp.html
+echo.> string/z.html
+echo.> string/hash.html
+echo.> string/manacher.html
+echo.> string/aho.html
+
+echo Setup complete!
+pause
